4bdc14a974f8d937200bcafb0aa1f0be09411d2a
Gunhad Arora <gunhadarora44@gmail.com>
2024-01-24 17:45:55 +0530

    time fix 2

--- lib/models/thread.model.ts
+++ lib/models/thread.model.ts
@@ -1,12 +1,11 @@
 import mongoose from "mongoose";
-import moment from "moment-timezone";
 
 
 const threadSchema = new mongoose.Schema({
   text: { type: String, required: true },
   author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   community: { type: mongoose.Schema.Types.ObjectId, ref: "Community" },
-  createdAt: { type: Date, default: moment().tz('Asia/Kolkata').toDate() },
+  createdAt: { type: Date, default: Date.now },
   parentId: { type: String },
   children: [
     {
